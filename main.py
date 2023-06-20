02d88e1cbf33165df2ed25899b8854ebaf446125
11KiRill11 <dolgushinkiya@gmail.com>
2023-06-20 21:37:44 +0500

    Добавлен класс змейки.

--- main.py
+++ main.py
@@ -28,3 +28,51 @@ speeds = {
     'medium': 15,
     'hard': 20
 }
+
+class Snake:
+
+    def __init__(self):
+
+        self.length = 1
+        self.positions = [((window_width // 2), (window_height // 2))]
+        self.direction = random.choice([up, down, left, right])
+        self.color = red
+        self.speed = speeds['medium']
+
+    def get_head_position(self):
+
+        return self.positions[0]
+
+    def move(self):
+
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0] + (x * gridsize)) % window_width), (cur[1] + (y * gridsize)) % window_height)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def reset(self):
+
+        self.length = 1
+        self.positions = [((window_width // 2), (window_height // 2))]
+        self.direction = random.choice([up, down, left, right])
+
+    def draw(self, surface):
+
+        for p in self.positions:
+            pygame.draw.rect(surface, self.color, pygame.Rect(p[0], p[1], gridsize, gridsize))
+
+    def handle_keys(self, keys):
+
+        if keys[pygame.K_UP] or keys[ord('w')]:
+            self.direction = up
+        elif keys[pygame.K_DOWN] or keys[ord('s')]:
+            self.direction = down
+        elif keys[pygame.K_LEFT] or keys[ord('a')]:
+            self.direction = left
+        elif keys[pygame.K_RIGHT] or keys[ord('d')]:
+            self.direction = right
